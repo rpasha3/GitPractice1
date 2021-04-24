f39b0451d6793a3d9250c67b7de9ab6bbfb5464f
raif pasha <rpasha3@hotmail.com>
2021-04-24 16:52:39 -0400

    added more work after publishing to hithub

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+        System.out.println("Adding GitHun Now");
+        System.out.println("YAY STUFF IS ON GITHUB");
 
     }
 }
